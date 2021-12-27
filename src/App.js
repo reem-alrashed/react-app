4d136aa99bb70891caf39222d1fc98c6b0c031f2
Reem AlRashed <reemalrashed2@gmail.com>
2021-12-27 15:23:05 +0300

    Fixed login page routing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
-
 import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-import logo from './logo.svg';
-import {Container,Navbar,Nav} from 'react-bootstrap';
+import {Container,Navbar} from 'react-bootstrap';
 import Login from './login';
+import { Routes, Route, Link } from "react-router-dom";
 
  
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <BrowserRouter>
         <div>
           <div className="header">
           <br />
-  <Navbar bg="light" variant="light" fixed="top" >
+     <Navbar bg="light" variant="light" fixed="top" >
     <Container>
     <Navbar.Brand href="#home">MyApp!</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link exact activeClassName="active" to="/">Home</Nav.Link>
-      <Nav.Link activeClassName="active" to="/login">Login</Nav.Link>
-    </Nav>
+      <nav>
+        <Link to="/login">Login</Link>
+      </nav>
     </Container>
   </Navbar>
-  <Nav className="me-auto">
-            {/* <NavLink exact activeClassName="active" to="/">Home</NavLink> */}
-            <NavLink activeClassName="active" to="/login">Login</NavLink>
-            {/* <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small> */}
-        </Nav>
           </div>
-          <div className="content">
-            <Switch>
-              <Route path="/login" component={Login} />
-            </Switch>
-          </div>
+
         </div>
-      </BrowserRouter>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+      <BrowserRouter>
     <App />
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
