c3516851641c5a30f1cc4eb440f1620e93852ea8
Reem AlRashed <reemalrashed2@gmail.com>
2021-12-27 14:36:07 +0300

    added router and bootstrap backage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import logo from './logo.svg';
+import {Container,Navbar,Nav} from 'react-bootstrap';
+import Login from './login';
+
+ 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <BrowserRouter>
+        <div>
+          <div className="header">
+          <br />
+  <Navbar bg="light" variant="light" fixed="top" >
+    <Container>
+    <Navbar.Brand href="#home">MyApp!</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link exact activeClassName="active" to="/">Home</Nav.Link>
+      <Nav.Link activeClassName="active" to="/login">Login</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <Nav className="me-auto">
+            {/* <NavLink exact activeClassName="active" to="/">Home</NavLink> */}
+            <NavLink activeClassName="active" to="/login">Login</NavLink>
+            {/* <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small> */}
+        </Nav>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
       </header>
     </div>
   );
 }
-
+ 
 export default App;
